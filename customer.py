10f20c954270e5168d439f1a4c66c082da098d74
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-14 07:25:51 +0300

    rectifies code for setting name

--- customer.py
+++ customer.py
@@ -1,6 +1,3 @@
-
-
-
 class Customer:
     def __init__(self, name):
         self.name = name
@@ -11,20 +8,23 @@ class Customer:
 
     @name.setter
     def name(self, value):
-        if not isinstance(value, str) and len(value) < 1 or len(value) > 15:
+        if not isinstance(value, str) or not (1 <= len(value) <= 15):
             raise TypeError("Name must be a string between 1 and 15 characters")
         self._name = value
 
     def orders(self):
         from order import Order
+
         return [order for order in Order.all_customers if order.customer == self]
 
     def coffees(self):
         from order import Order
+
         return list(
             {order.coffee for order in Order.all_customers if order.customer == self}
         )
 
     def create_order(self, coffee, price):
         from order import Order
+
         return Order(self, coffee, price)
